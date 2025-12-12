c73894b6cd9c384293c96f6a11e18819228867fe
Ufuk Turuncoglu <turuncu@ucar.edu>
2025-12-12 10:29:53 -0700

    make aurora two-way coupled configuration default

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Main script to build application DLROMS: [CDEPS, GeoGate, ROMS]
+# -DDEBUG=ON can be passed to GeoGate build_args to enable debugging
 
+# Load environment
 source envs/derecho_env_gnu.sh
+
+# Clean old build
 rm -rf build install esmxBuild.yaml
+
+# Create esmxBuild.yaml
 echo "application:" >> esmxBuild.yaml 
 echo "  disable_comps: ESMX_Data" >> esmxBuild.yaml
 echo "  link_libraries: piof conduit catalyst catalyst_fortran python3.12" >> esmxBuild.yaml
@@ -15,7 +22,7 @@ echo "    libraries: datm dshr streams cdeps_share" >> esmxBuild.yaml
 echo "  geogate:" >> esmxBuild.yaml
 echo "    source_dir: src/GeoGate/src" >> esmxBuild.yaml
 echo "    build_type: cmake.external" >> esmxBuild.yaml
-echo "    build_args: \"-DGEOGATE_USE_PYTHON=ON -DGEOGATE_USE_CATALYST=ON -DCMAKE_Fortran_FLAGS=-ffree-line-length-none\"" >> esmxBuild.yaml
+echo "    build_args: \"-DGEOGATE_USE_PYTHON=ON -DGEOGATE_USE_CATALYST=ON\"" >> esmxBuild.yaml
 echo "    fort_module: geogate_nuopc.mod" >> esmxBuild.yaml
 echo "    libraries: geogate geogate_io geogate_python geogate_catalyst geogate_shared" >> esmxBuild.yaml
 echo "  roms:" >> esmxBuild.yaml
@@ -24,4 +31,6 @@ echo "    build_type: cmake.external" >> esmxBuild.yaml
 echo "    build_args: \"-DROMS_EXECUTABLE=OFF -DLIBTYPE=STATIC -DROMS_SRC_DIR=src/ROMS -DMY_HEADER_DIR=../../../apps -DROMS_APP=IRENE -DMPI=ON\"" >> esmxBuild.yaml
 echo "    fort_module: cmeps_roms_mod" >> esmxBuild.yaml
 echo "    libraries: ROMS" >> esmxBuild.yaml
+
+# Build application
 ESMX_Builder -v --build-jobs=4 --build-args="-DCMAKE_Fortran_FLAGS=-I${ESMF_ROOT}/include" --cmake-args="-DCMAKE_Fortran_FLAGS=-I${PWD}/build/roms/module"
